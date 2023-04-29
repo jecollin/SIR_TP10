7a6f5c036a24e1e9fccaea52ab7c1ba7627c5767
Jean-Baptiste <jean-baptiste.collin@etudiant.univ-rennes1.fr>
2023-04-29 23:03:59 +0200

    modif arbre

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import {FormsModule} from "@angular/forms";
 import { TicketComponent } from './ticket/ticket.component';
+import { UserComponent } from './user/user.component';
+import { TicketCommentComponent } from './ticket-comment/ticket-comment.component';
+import { BugTicketComponent } from './bug-ticket/bug-ticket.component';
+import { FeatureRequestTicketComponent } from './feature-request-ticket/feature-request-ticket.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TicketComponent
+    TicketComponent,
+    UserComponent,
+    TicketCommentComponent,
+    BugTicketComponent,
+    FeatureRequestTicketComponent
   ],
     imports: [
         BrowserModule,
         FormsModule
     ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: []
 })
 export class AppModule { }
