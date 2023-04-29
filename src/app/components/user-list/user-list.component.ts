5696dd0d77a560dce78b0ec831b96c9dc8f07f24
Jean-Baptiste <jean-baptiste.collin@etudiant.univ-rennes1.fr>
2023-04-29 23:49:58 +0200

    modif

--- src/app/components/user-list/user-list.component.ts
+++ src/app/components/user-list/user-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
-import { User } from '../user';
+import { User } from '../src/app/models/user';
 import { UserService } from '../user.service';
 
 @Component({
