ec5141cb71b909331e6e48bfe3ebdd687685a088
Jean-Baptiste <jean-baptiste.collin@etudiant.univ-rennes1.fr>
2023-04-30 01:32:31 +0200

    correction errors

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { TicketListComponent } from '../ticket-list/ticket-list';
+import { TicketListComponent } from './ticket-list/ticket-list.component';
 
 const routes: Routes = [
     { path: 'tickets', component: TicketListComponent },
